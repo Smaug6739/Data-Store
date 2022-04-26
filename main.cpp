19f93f9a50b9a5d40acb8ac1595985f811e07630
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-26 15:10:57 +0200

    Rename

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Data store.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
